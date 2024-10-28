80465e067eedc950e7d202ad05c9fe616e9c4d1e
K123Pz123 <geed100ff@gmail.com>
2024-10-28 10:45:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["dc.gg/crazyshop", "dc.gg/crazyshop"];
+const statusMessages = ["dc.gg/coldshop", "dc.gg/coldshop"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
